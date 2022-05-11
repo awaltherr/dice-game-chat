7158e825231df11ff2af37acf2f0e8d9426005b7
Anton Walther <anton.walther@live.com>
2022-05-11 14:50:21 +0200

    Sorting out Socket connection

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-const server = require('http').createServer(app);
+const http = require('http');
+const path = require('path');
+const {
+    Server
+} = require('socket.io');
+const fs = require('fs')
+
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
+    fs.readFile('index.html', function (error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('Error: File Not Found')
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
+});
+
+const io = new Server(server)
 
 app.use(express.static(path.join(__dirname + '/public')));
 
-server.listen(3000);
+io.on('connection', socket => {
+    console.log('New User Connection...');
+    socket.emit('message', 'Välkommen till oss!');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT)
+})
